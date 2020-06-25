0cb6799ba68c6e8812cae95ca79cc6b58323282a
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-06-25 19:29:34 +0000

    have tested the app after flask installed to ensure that is evrything is working

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Test the app after installed flask"
+ 
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
+
