aa70d44491dfe7960a55fc7d5c6a7a55afc91b2c
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-07-01 20:01:44 +0000

    add update function to productions template to update if there any changes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
@@ -17,6 +18,21 @@ def get_productions():
     return render_template("productions.html",
                            productions=mongo.db.productions.find(),
                            employees=mongo.db.employees.find())
+
+
+@app.route("/update_production/<production_id>", methods=["POST"])
+def update_production(production_id):
+    productions = mongo.db.productions
+    productions.update({"_id": ObjectId(production_id)},
+    {
+         'product_name': request.form.get('product_name'),
+         'machine_name': request.form.get('machine_name'),
+         'product_number': request.form.get('product_number'),
+         'employees': request.form.get('employees'),
+         'comments': request.form.get('comments'),
+         'date': request.form.get('date')
+    })
+    return redirect(url_for("get_productions"))
 
 
 if __name__ == "__main__":
