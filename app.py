281dfc701d105f368613e212fc533d1ceecb9e77
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-07-05 21:37:33 +0000

    make a function and template for employees record

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ def get_productions():
 @app.route("/add_production")
 def add_production():
     return render_template("addproduction.html",
-                           employees=mongo.db.employees.find(),
                            products=mongo.db.products.find(),
                            machines=mongo.db.machines.find())
 
@@ -84,8 +83,7 @@ def get_products():
 
 @app.route("/add_product")
 def add_product():
-    return render_template("addproduct.html",
-                           products=mongo.db.products.find())
+    return render_template("addproduct.html")
 
 
 @app.route("/insert_product", methods=["POST"])
@@ -125,8 +123,7 @@ def get_machines():
 
 @app.route("/add_machine")
 def add_machine():
-    return render_template("addmachine.html",
-                           machines=mongo.db.machines.find())
+    return render_template("addmachine.html")
 
 
 @app.route("/insert_machine", methods=["POST"])
@@ -159,6 +156,48 @@ def update_machine(machine_id):
 def delet_machine(machine_id):
     mongo.db.machines.remove({"_id": ObjectId(machine_id)})
     return redirect(url_for("get_machines"))
+
+
+# -------Employee(CRUD)-------------
+@app.route("/get_employees")
+def get_employees():
+    return render_template("employees.html",
+                           employees=mongo.db.employees.find())
+
+
+@app.route("/add_employee")
+def add_employee():
+    return render_template("addemployee.html")
+
+
+@app.route("/insert_employee", methods=["POST"])
+def insert_employee():
+    employees = mongo.db.employees
+    employees.insert_one(request.form.to_dict())
+    return redirect(url_for("get_employees"))
+
+
+@app.route('/edit_employee/<employee_id>')
+def edit_employee(employee_id):
+    the_employee = mongo.db.employees.find_one({"_id": ObjectId(employee_id)})
+    return render_template('editemployee.html', employee=the_employee)
+
+
+@app.route("/update_employee/<employee_id>", methods=["GET", "POST"])
+def update_employee(employee_id):
+    employees = mongo.db.employees
+    employees.update({"_id": ObjectId(employee_id)},
+    {
+         'first_name': request.form.get('first_name'),
+         'last_name': request.form.get('last_name'),
+         'nick_name': request.form.get('nick_name')
+    })
+    return redirect(url_for("get_employees"))
+
+@app.route('/delet_employee/<employee_id>')
+def delet_employee(employee_id):
+    mongo.db.employees.remove({"_id": ObjectId(employee_id)})
+    return redirect(url_for("get_employees"))
 
 
 if __name__ == "__main__":
