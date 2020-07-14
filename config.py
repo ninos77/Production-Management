e1acc152b82337ea920d497f0c8b71f49a4b886b
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-07-14 16:33:10 +0000

    Add config file

---
+++ config.py
@@ -0,0 +1,6 @@
+
+
+class Config(object):
+
+    MONGO_DBNAME = "Production_Management"
+    MONGO_URI = "mongodb+srv://ninos77:Itanaeliana20122016@myfirstcluster-fsr8d.mongodb.net/Production_Management?retryWrites=true&w=majority"
