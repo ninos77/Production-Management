d8133845bd48e02aeeffb7db02aa51e33f473328
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-07-14 20:31:36 +0000

    modified on my .gitignore

--- config.py
+++
@@ -1,6 +0,0 @@
-
-
-class Config(object):
-
-    MONGO_DBNAME = "Production_Management"
-    MONGO_URI = "mongodb+srv://ninos77:Itanaeliana20122016@myfirstcluster-fsr8d.mongodb.net/Production_Management?retryWrites=true&w=majority"
